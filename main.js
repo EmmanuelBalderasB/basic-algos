a47d69e7b35a846f2422531811831b48ce82f8fe
EmmanuelBalderasB <ebalderasbeq@gmail.com>
2022-10-15 05:10:26 -0500

    formats code

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function titleCase(str) {
     const arr = str.split(' ');                             //Make an array in which each word in str is a separate index
-    let uppCase = []                                        //init empty array
+    let uppCase = [];                                      //init empty array
     for (const word of arr) {                               //iterate through each item of arr
       let i = word.slice(0, 1).toUpperCase();               //first character of each item selected and upper cased, saved to i variable
       let j = word.slice(1).toLowerCase();                  //every character except first one is selected and lower cased, saved to j variable
       uppCase.push(i + j)                                   //push variables into empty array per iteration
-      
+
     }
     const result = uppCase.join(' ');                       //separate result array back into a string
     return result;
@@ -21,7 +21,7 @@ for (let i = 0; i < 10; i++) {
         //result.splice(n,0,arr)
         return result;
 }
-    
+
     console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
     console.timeEnd()
 
@@ -56,5 +56,5 @@ function bouncer(arr) {
     }
     return result;
   };
-  
+  console.log(titleCase('WonDeRinG'));
   console.log(bouncer([7, "ate", "", false, 9]));
